c18da356eeb217203b94d7eb62e1bca2fdc6610e
Andrew <messerback@hotmail.com>
2018-05-05 15:10:04 +0300

    Styling

--- public/js/index.js
+++ public/js/index.js
@@ -32,12 +32,14 @@ socket.on("newLocationMessage", function(message){
 
 jQuery("#message-form").on("submit", function(e) {
     e.preventDefault();
+    
+    var textBox = jQuery("[name=message]");
 
     socket.emit("createMessage", {
         from: "User",
-        text: jQuery("[name=message]").val()
+        text: textBox.val()
     }, function(){
-
+        textBox.val("");
     });
 });
 
@@ -47,12 +49,16 @@ locationButton.on("click", function() {
        return alert("Geolocation is not supported!");
     } 
 
+    locationButton.attr("disabled", "disabled").text("Sending location...");
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr("disabled").text("Send location");
         socket.emit("createLocationMessage", {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function() {
+        locationButton.removeAttr("disabled").text("Send location");
         alert("Unable to fetch location");
     });
     
